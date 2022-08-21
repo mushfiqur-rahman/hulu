843e9030aced07fd7272939f0c4c1b0b5b3905e1
mushfiq-rahman <mushfiq.style@gmail.com>
2022-08-21 23:48:34 +0600

    first commit

---
+++ components/Header.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import React from "react";
+
+import Logo from "../public/Hulu_Logo.svg";
+import HeaderItem from "./HeaderItem";
+
+import {
+  HomeIcon,
+  LightningBoltIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+function Header() {
+  return (
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+      <Image src={Logo} alt={Logo} width={200} height={35} />
+    </header>
+  );
+}
+
+export default Header;
